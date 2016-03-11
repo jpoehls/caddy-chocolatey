f5b76f037a8c6297563dbe2ec2e2da3b78fd8963
Joshua Poehls <joshua.poehls@interworks.com>
2016-03-10 20:41:22 -0600

    Rebuilt caddy package to bundle the exactly release version advertised.

--- build.ps1
+++ build.ps1
@@ -1,18 +1,64 @@
-$latest = Invoke-WebRequest https://api.github.com/repos/mholt/caddy/releases/latest | select -ExpandProperty Content | ConvertFrom-Json
-$version = $latest.tag_name.TrimStart('v').Trim()
-$releaseNotes = $latest.body.Trim()
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
 
-Write-Output "Latest Caddy version: $version"
+function Update-NuSpec {
+    param(
+        [string]$Path,
+        [string]$Version,
+        [string]$ReleaseNotes
+    )
 
-$nuspecFile = Join-Path (Split-Path $PSCommandPath) 'caddy.nuspec'
+    $ns = @{ nuspec = 'http://schemas.microsoft.com/packaging/2015/06/nuspec.xsd' }
+    $xml = [xml](Get-Content -LiteralPath $Path -Raw)
+   
+    $el = $xml | Select-Xml -XPath '/nuspec:package/nuspec:metadata/nuspec:version' -Namespace $ns
+    $el.Node.InnerText = $Version
 
-$ns = @{ nuspec = 'http://schemas.microsoft.com/packaging/2015/06/nuspec.xsd' }
-$versionEl = Select-Xml -LiteralPath $nuspecFile -XPath '/nuspec:package/nuspec:metadata/nuspec:version' -Namespace $ns
-$versionEl.Node.InnerText = $version
-$versionEl.Node.OwnerDocument.Save($nuspecFile)
+    $el = $xml | Select-Xml -XPath '/nuspec:package/nuspec:metadata/nuspec:releaseNotes' -Namespace $ns
+    $el.Node.InnerText = $ReleaseNotes
 
-$versionEl = Select-Xml -LiteralPath $nuspecFile -XPath '/nuspec:package/nuspec:metadata/nuspec:releaseNotes' -Namespace $ns
-$versionEl.Node.InnerText = $releaseNotes
-$versionEl.Node.OwnerDocument.Save($nuspecFile)
+    $xml.Save($Path)
+}
 
-choco pack
+function Get-Caddy {
+    param(
+        [string]$Arch
+    )
+
+    $features = [string]::Join("%2C", @("cors"; "git"; "hugo"; "ipfilter"; "jsonp"; "mailout"; "search"))
+    $url = "https://caddyserver.com/download/build?os=windows&arch=$Arch&features=$features"
+    $file = Join-Path (Split-Path $PSCommandPath) "tools/caddy-$Arch.zip"
+    
+    Invoke-WebRequest -Uri $url -OutFile $file
+}
+
+function Get-CaddyReleaseNotes {
+    param(
+        [string]$Version
+    )
+
+    $resp = Invoke-WebRequest "https://api.github.com/repos/mholt/caddy/releases/tags/v$Version" | select -ExpandProperty Content | ConvertFrom-Json
+    return $resp.body.Trim()
+}
+
+$resp = Invoke-WebRequest -Uri 'https://caddyserver.com/download'
+if (-not($resp -match 'Version\s+(\d+\.\d+\.\d+)')) {
+    throw 'Failed to find the version number.'
+}
+$version = $Matches[1]
+Write-Output "Caddy Version: $version"
+
+Write-Output "Downloading 32-bit..."
+Get-Caddy 386
+Write-Output "Downloading 64-bit..."
+Get-Caddy amd64
+
+Write-Output "Fetching release notes..."
+$releaseNotes = Get-CaddyReleaseNotes $version
+
+Write-Output "Updating the NuSpec...."
+$nuspec = Join-Path (Split-Path $PSCommandPath) 'caddy.nuspec'
+Update-NuSpec -Path $nuspec -Version $version -ReleaseNotes $releaseNotes
+
+Write-Output "Packing..."
+choco pack $nuspec
--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
-$installDir = Split-Path -parent $MyInvocation.MyCommand.Definition
- 
-Install-ChocolateyZipPackage `
-    -packageName 'caddy' `
-    -url 'https://caddyserver.com/download/build?os=windows&arch=386&features=cors%2Cgit%2Chugo%2Cipfilter%2Cjsonp%2Cmailout%2Csearch' `
-    -url64bit 'https://caddyserver.com/download/build?os=windows&arch=amd64&features=cors%2Cgit%2Chugo%2Cipfilter%2Cjsonp%2Cmailout%2Csearch' `
-    -unzipLocation $installDir
+$installDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$zipFile = if (Get-ProcessorBits 32) {
+    Join-Path $installDir "caddy-386.zip"
+} else {
+    Join-Path $installDir "caddy-amd64.zip"
+}
+
+Get-ChocolateyUnzip $zipFile $installDir
